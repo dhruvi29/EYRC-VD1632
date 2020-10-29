b8be495e8c3ba1bc2357107b3f9f0ee4c9cdb970
[Karthik Swaminathan] <[karthikswami.1802@gmail.com]>
2020-10-30 00:56:39 +0530

    Latest commit

--- position_controller.py
+++ position_controller.py
@@ -20,7 +20,7 @@ class edrone():
 		self.attitude_cmd.rcRoll=1500.0
 		self.attitude_cmd.rcPitch=1500.0
 		self.attitude_cmd.rcYaw=1500.0
-		self.attitude_cmd.rcThrottle=1500.0
+		self.attitude_cmd.rcThrottle=1490.0
 		
 		
 		#self.attitude_cmd.aux1=0.0
@@ -35,14 +35,14 @@ class edrone():
 		self.altitude_range_pErr = 0.0
 		self.altitude_range_output = 0.0
 		self.altitude_range_dErr = 0.0
-		self.altitude_range_Kp = 1
-		self.altitude_range_Kd = 1
-		self.altitude_range_Ki = 1
+		self.altitude_range_Kp = 0
+		self.altitude_range_Kd = 0
+		self.altitude_range_Ki = 0
 		
 		
-		self.Kp=[1,1,1]
-		self.Kd=[1,1,1]
-		self.Ki=[1,1,1]
+		self.Kp=[0.0,0.0,0.0]
+		self.Kd=[0.0,0.0,0.0]
+		self.Ki=[0.0,0.0,0.0]
 
 		self.error=[0.0,0.0,0.0]
 		self.prev_error=[0.0,0.0,0.0]
@@ -51,7 +51,7 @@ class edrone():
 		self.output=[0.0,0.0,0.0]
 		self.dErr=[0.0,0.0,0.0]
 
-		self.sample_time = 1
+		self.sample_time = 0.5
 		
 		#check for Float64
 		
@@ -96,8 +96,6 @@ class edrone():
 		self.altitude_range_Ki=msg.Ki*0.008
 		self.altitude_range_Kd=msg.Kd*0.3
 
-		
-
     
 
 	def pid(self):
@@ -105,6 +103,7 @@ class edrone():
 		self.error[1]=self.set_point[1]-self.curr_point[1]
 		self.error[2]=self.set_point[2]-self.curr_point[2]
 		self.altitude_range_error=self.altitude_range_set - self.altitude_range_curr
+		print(self.error[2])
 		
 		#proportional error
 		self.pErr[0]=self.Kp[0]*(self.error[0])
@@ -126,19 +125,19 @@ class edrone():
 		
 		self.output[0]=self.pErr[0] + self.Ki[0]*self.iterm[0]*self.sample_time + self.Kd[0]*self.dErr[0]
 		self.output[1]=self.pErr[1] + self.Ki[1]*self.iterm[1]*self.sample_time + self.Kd[1]*self.dErr[1]
-		self.output[2]=self.pErr[2] + self.Ki[2]*self.iterm[2]*self.sample_time + self.Kd[2]*self.dErr[2]
-		self.altitude_range_output=self.altitude_range_pErr +(self.altitude_range_Ki*self.altitude_range_iterm)*self.sample_time + self.altitude_range_Kd*self.altitude_range_dErr
+		self.output[2]=self.pErr[2] + self.Ki[2]*self.iterm[2]*self.sample_time + self.Kd[2]*self.dErr[2] + self.attitude_cmd.rcThrottle
+		self.altitude_range_output=self.altitude_range_pErr +(self.altitude_range_Ki*self.altitude_range_iterm)*self.sample_time + self.altitude_range_Kd*self.altitude_range_dErr + self.attitude_cmd.rcThrottle
 		
 		#if self.output[0]>1024: self.output[0]=1024
 		#if self.output[1]>1024: self.output[1]=1024
-		#if self.output[2]>1024: self.output[2]=1024
+		if self.output[2]>1510: self.output[2]=1510
 		
 		#if self.output[0]<0: self.output[0]=0
 		#if self.output[1]<0: self.output[1]=0
-		#if self.output[2]<0: self.output[2]=0
+		if self.output[2]<1490: self.output[2]=1490
 		
 		#updating output throttle(thrust)
-		self.attitude_cmd.rcThrottle = self.altitude_range_output
+		self.attitude_cmd.rcThrottle = self.output[2]
 		#updating error
 		self.prev_error[0]=self.error[0]
 		self.prev_error[1]=self.error[1]
@@ -150,14 +149,15 @@ class edrone():
 		self.drone_cmd_pub.publish(self.attitude_cmd)
 		self.latitude_error_pub.publish(self.error[0])
 		self.longitude_error_pub.publish(self.error[1])
-		self.altitude_error_pub.publish(self.altitude_range_error)
+		self.altitude_error_pub.publish(self.error[2])
 		
 		
 #exception handling
 if __name__ == '__main__':
 
 	e_drone = edrone()
-	r = rospy.Rate(e_drone.sample_time)  # specify rate in Hz based upon your desired PID sampling time, i.e. if desired sample time is 33ms specify rate as 30Hz
+	r = rospy.Rate(1/e_drone.sample_time)  # specify rate in Hz based upon your desired PID sampling time, i.e. if desired sample time is 33ms specify rate as 30Hz
 	while not rospy.is_shutdown():
 		e_drone.pid()
 		r.sleep()
+
